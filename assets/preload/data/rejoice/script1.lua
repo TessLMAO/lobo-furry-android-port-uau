3df3f4e0db971c92ba05ed761da8dc7efc884997
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-12-26 18:49:19 -0300

    Add files via upload

---
+++ assets/preload/data/rejoice/script1.lua
@@ -0,0 +1,11 @@
+
+function oncreate()
+   		for i = 0, getProperty('opponentStrums.length')-1 do
+			setPropertyFromGroup('opponentStrums', i, 'texture', 'lyc');
+		end
+		for i = 0, getProperty('unspawnNotes.length')-1 do
+			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') == false then
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'lyc');
+			end
+		end
+	end 
