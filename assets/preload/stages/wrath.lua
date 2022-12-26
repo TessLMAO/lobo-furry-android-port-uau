d0a7fc0324df844c7988be990bff8d78f32722c2
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-12-26 19:01:28 -0300

    Add files via upload

---
+++ assets/preload/stages/wrath.lua
@@ -0,0 +1,23 @@
+
+function onCreate()
+	-- background shit
+
+
+
+
+makeLuaSprite('bg', '1/bg', -1050, -1330);
+	addLuaSprite('bg', 'bg', false);
+    setLuaSpriteScrollFactor('bg', 1.2, 1.1)
+    scaleObject('bg', 1.0, 1.0) 
+
+
+    makeLuaSprite('b1', '1/z', -900, -1120);
+  addLuaSprite('b1', 'b1', false);
+    scaleObject('b1', 1.0, 1.0) 
+
+
+
+
+
+
+end    
